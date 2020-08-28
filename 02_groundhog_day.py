5d2c59f904e84aa9987db13886c23047aac6bae8
K-Roman <cawme@mail.ru>
2020-08-28 18:09:02 +0300

    gotovo

--- 02_groundhog_day.py
+++ 02_groundhog_day.py
@@ -17,7 +17,127 @@
 # При создании собственных исключений максимально использовать функциональность
 # базовых встроенных исключений.
 
-ENLIGHTENMENT_CARMA_LEVEL = 777
+import random
+import time
+
+# class IamGodError(Exception):
+#     def __init__(self, message):
+#         self.message = message
+#
+#     def __str__(self):
+#         return self.message
+#
+# class DrunkError(Exception):
+#     def __init__(self, message):
+#         self.message = message
+#
+#     def __str__(self):
+#         return self.message
+#
+# class CarCrashError(Exception):
+#     def __init__(self, message):
+#         self.message = message
+#
+#     def __str__(self):
+#         return self.message
+#
+# class GluttonyError(Exception):
+#     def __init__(self, message):
+#         self.message = message
+#
+#     def __str__(self):
+#         return self.message
+#
+# class DepressionError(Exception):
+#     def __init__(self, message):
+#         self.message = message
+#
+#     def __str__(self):
+#         return self.message
+#
+# class SuicideError(Exception):
+#     def __init__(self, message):
+#         self.message = message
+#
+#     def __str__(self):
+#         return self.message
+
+class IamGodError(BaseException):
+    def __init__(self):
+        self.message = 'Я понял - я бог'
+        self.time = time.ctime(time.time())
+
+    def __str__(self):
+        return self.message
+
+class DrunkError(BaseException):
+    def __init__(self):
+        self.message = 'буду просто пить, завтра не будет похмелья'
+        self.time = time.ctime(time.time())
+
+    def __str__(self):
+        return self.message
+
+
+class CarCrashError(BaseException):
+    def __init__(self):
+        self.message = 'разобьюсь и ладно'
+        self.time = time.ctime(time.time())
+
+    def __str__(self):
+        return self.message
+
+class GluttonyError(BaseException):
+    def __init__(self):
+        self.message = 'обжорство не проблема'
+        self.time = time.ctime(time.time())
+
+    def __str__(self):
+        return self.message
+
+class DepressionError(BaseException):
+    def __init__(self):
+        self.message = 'я никогда не выберусь отсюда'
+        self.time = time.ctime(time.time())
+
+    def __str__(self):
+        return self.message
+
+class SuicideError(BaseException):
+    def __init__(self):
+        self.message = 'время пришло'
+        self.time = time.ctime(time.time())
+
+    def __str__(self):
+        return self.message
+
+
+
+def one_day():
+    global ENLIGHTENMENT_CARMA_LEVEL
+    global errors
+    ENLIGHTENMENT_CARMA_LEVEL += random.randint(1,7)
+    dice = random.randint(1,13)
+    errors = [IamGodError(), DrunkError(), CarCrashError(),
+              GluttonyError(), DepressionError(), SuicideError()]
+    if dice == 1:
+        with open('log12.txt', mode='a', encoding='utf8') as file:
+            a = random.choice(errors)
+            file.write(str(type(a)) + str(a.message) + ' ' + str(a.time)+ "\n")
+            raise a
+
+    return ENLIGHTENMENT_CARMA_LEVEL
+
+
+ENLIGHTENMENT_CARMA_LEVEL = 0
+
+while ENLIGHTENMENT_CARMA_LEVEL < 777:
+    try:
+        one_day()
+    except (IamGodError, DrunkError, CarCrashError,
+                      GluttonyError, DepressionError, SuicideError) as exc:
+        print(f'Сегодня {exc}')
+    print(ENLIGHTENMENT_CARMA_LEVEL)
 
 # TODO здесь ваш код
 
--- 03_registration_log.py
+++ 03_registration_log.py
@@ -23,3 +23,55 @@
 # Вызов метода обернуть в try-except.
 
 # TODO здесь ваш код
+
+
+def regist(line):
+    name, email, age = line.split(' ')
+    # print(name, email, age)
+    if name is None or email is not None or age is not None:
+        pass
+    else:
+        raise ValueError('отсутствует одно из полей')
+
+    if name.isalpha():
+        data = name
+    else:
+        raise NotNameError("поле имени содержит что-то кроме букв")
+
+    if "@" in email and '.' in email:
+        data += ' ' + email
+    else:
+        raise NotEmailError('что-то не так с почтой')
+    if 10 < int(age) <= 99:
+        data += ' ' + age
+    else:
+        raise ValueError('что-то не так с возрастом')
+    return data
+
+
+
+
+class NotNameError(Exception):
+    pass
+
+
+class NotEmailError(Exception):
+    pass
+number = 0
+with open('registrations.txt', 'r', encoding='utf8') as ff:
+    for line in ff:
+        number += 1
+        try:
+            da = regist(line)
+            with open("registrations_good.log", 'a', encoding='utf8') as f1:
+                f1.write(f'{da[:-1]} номер строки {number}' + '\n')
+                print(da[:-1], str(number))
+        except (ValueError, NotNameError, NotEmailError) as exc:
+            with open("registrations_bad.txt", 'a', encoding='utf8') as f2:
+                if "unpack" in exc.args[0]:
+                    f2.write(f'ошибка отсутствует одно из полей в строке {line[:-1]} под номером {number} \n')
+                    print(f'ошибка отсутствует одно из полей в строке {line[:-1]} под номером {number}')
+                else:
+                    f2.write(f'ошибка {exc} в строке {line[:-1]} под номером {number}' + '\n')
+                    print(f'ошибка {exc} в строке {line[:-1]} под номером {number}')
+
